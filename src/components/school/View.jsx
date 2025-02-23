e59029fb834275cb66c2907baa5756e652edac28
Shihab-md <nmd.shihabudeen@gmail.com>
2025-02-24 02:12:11 +0900

    Update View.jsx

--- src/components/school/View.jsx
+++ src/components/school/View.jsx
@@ -33,7 +33,7 @@ const View = () => {
     <>
       {school ? (
         <div className="max-w-3xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md">
-          <h2 className="text-2xl font-bold mb-8 text-center">
+          <h2 className="text-2xl font-bold mb-6 text-center">
             School Details
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-0">
