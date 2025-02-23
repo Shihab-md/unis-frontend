1e681df3be263e99f0c0841bcce53502dd215f20
Shihab-md <nmd.shihabudeen@gmail.com>
2025-02-24 02:19:10 +0900

    Update View.jsx

--- src/components/school/View.jsx
+++ src/components/school/View.jsx
@@ -32,18 +32,55 @@ const View = () => {
   return (
     <>
       {school ? (
-        <div className="max-w-3xl mx-auto mt-6 bg-white p-6 rounded-md shadow-md">
-          <h2 className="text-2xl font-bold mb-6 text-center">
-            School Details
+        <div className="max-w-3xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md">
+          <h2 className="text-2xl font-bold mb-8 text-center">
+            School Detail
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-0">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div>
-              <h3 className="text-lg font-bold text-left">Code:</h3></div>
-            <div><h4 className="font-medium text-left">{school.code}</h4>
+
             </div>
             <div>
-              <p className="text-lg font-bold text-left">Name:</p></div>
-            <div><p className="font-medium text-left">{school.name}</p>
+              <div className="flex space-x-3 mb-5">
+                <p className="text-lg font-bold">Code:</p>
+                <p className="font-medium">{school.code}</p>
+              </div>
+              <div className="flex space-x-3 mb-5">
+                <p className="text-lg font-bold">Name:</p>
+                <p className="font-medium">{school.name}</p>
+              </div>
+              <div className="flex space-x-3 mb-5">
+                <p className="text-lg font-bold">Address:</p>
+                <p className="font-medium">{school.address}</p>
+              </div>
+              <div className="flex space-x-3 mb-5">
+                <p className="text-lg font-bold">Contact Number:</p>
+                <p className="font-medium">{school.contactNumber}</p>
+              </div>
+              <div className="flex space-x-3 mb-5">
+                <p className="text-lg font-bold">Email:</p>
+                <p className="font-medium">{school.email}</p>
+              </div>
+              <div className="flex space-x-3 mb-5">
+                <p className="text-lg font-bold">Status:</p>
+                <p className="font-medium">{school.active}</p>
+              </div>
+              <div className="flex space-x-3 mb-5">
+                <p className="text-lg font-bold">Incharge1:</p>
+                <p className="font-medium">{school.incharge1}</p>
+              </div>
+              <div className="flex space-x-3 mb-5">
+                <p className="text-lg font-bold">Incharge1 Number:</p>
+                <p className="font-medium">{school.incharge1Number}</p>
+              </div>
+              <div className="flex space-x-3 mb-5">
+                <p className="text-lg font-bold">Incharge2:</p>
+                <p className="font-medium">{school.incharge2}</p>
+              </div>
+              <div className="flex space-x-3 mb-5">
+                <p className="text-lg font-bold">Incharge2 Number:</p>
+                <p className="font-medium">{school.incharge2Number}</p>
+              </div>
             </div>
           </div>
         </div>
