86712cffa34aeba9a5fc2d9fd410c0e72b0f6661
Shihab-md <nmd.shihabudeen@gmail.com>
2025-02-24 02:41:47 +0900

    Update Edit.jsx

--- src/components/school/Edit.jsx
+++ src/components/school/Edit.jsx
@@ -10,7 +10,7 @@ const Edit = () => {
     salary: 0,
     department: "",
   });
-  const [departments, setDepartments] = useState(null);
+
   const navigate = useNavigate();
   const { id } = useParams();
 
@@ -72,15 +72,30 @@ const Edit = () => {
 
   return (
     <>
-      {departments && school ? (
+      {school ? (
         <div className="max-w-4xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md">
           <h2 className="text-2xl font-bold mb-6">Edit School</h2>
           <form onSubmit={handleSubmit}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {/* Code */}
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Code
+                </label>
+                <input
+                  type="text"
+                  name="code"
+                  value={school.code}
+                  onChange={handleChange}
+                  placeholder="Insert Code"
+                  className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+                  required
+                />
+              </div>
               {/* Name */}
               <div>
                 <label className="block text-sm font-medium text-gray-700">
-                  Name
+                  Code
                 </label>
                 <input
                   type="text"
@@ -93,59 +108,136 @@ const Edit = () => {
                 />
               </div>
 
-              {/* Marital Status */}
+              {/* Address */}
               <div>
                 <label className="block text-sm font-medium text-gray-700">
-                  Marital Status
-                </label>
-                <select
-                  name="maritalStatus"
-                  onChange={handleChange}
-                  value={school.maritalStatus}
-                  placeholder="Marital Status"
-                  className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
-                  required
-                >
-                  <option value="">Select Status</option>
-                  <option value="single">Single</option>
-                  <option value="married">Married</option>
-                </select>
-              </div>
-
-              {/* Designation */}
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  Designation
+                  Address
                 </label>
                 <input
                   type="text"
-                  name="designation"
+                  name="address"
+                  value={school.address}
                   onChange={handleChange}
-                  value={school.designation}
-                  placeholder="Designation"
+                  placeholder="Insert Address"
                   className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
                   required
                 />
               </div>
 
-              {/* Salary */}
+              {/* Contact Number */}
               <div>
                 <label className="block text-sm font-medium text-gray-700">
-                  Salary
+                  Contact Number
+                </label>
+                <input
+                  type="text"
+                  name="contactNumber"
+                  value={school.contactNumber}
+                  onChange={handleChange}
+                  placeholder="Insert Contact Number"
+                  className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+                  required
+                />
+              </div>
+
+              {/* Email */}
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  value={school.email}
+                  onChange={handleChange}
+                  placeholder="Insert Email"
+                  className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+                  required
+                />
+              </div>
+
+              {/* Active */}
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Active
+                </label>
+                <select
+                  name="active"
+                  value={school.active}
+                  onChange={handleChange}
+                  className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+                  required
+                >
+                  <option value="">Select</option>
+                  <option value="Active">Active</option>
+                  <option value="InActive">In-Active</option>
+                </select>
+              </div>
+
+              {/* Incharge1 */}
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Incharge1 Name
+                </label>
+                <input
+                  type="text"
+                  name="incharge1"
+                  value={school.incharge1}
+                  onChange={handleChange}
+                  placeholder="Incharge1 Name"
+                  className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+                  required
+                />
+              </div>
+
+              {/* Incharge1 Number */}
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Incharge1 Number
                 </label>
                 <input
                   type="number"
-                  name="salary"
+                  name="incharge1Number"
+                  value={school.incharge1Number}
                   onChange={handleChange}
-                  value={school.salary}
-                  placeholder="Salary"
+                  placeholder="Incharge1 Number"
                   className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
                   required
                 />
               </div>
 
-              {/* Department */}
-                          </div>
+              {/* Incharge2 */}
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Incharge2 Name
+                </label>
+                <input
+                  type="text"
+                  name="incharge2"
+                  value={school.incharge2}
+                  onChange={handleChange}
+                  placeholder="Incharge2 Name"
+                  className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+                //required
+                />
+              </div>
+
+              {/* Incharge2 Number */}
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Incharge2 Number
+                </label>
+                <input
+                  type="number"
+                  name="incharge2Number"
+                  value={school.incharge2Number}
+                  onChange={handleChange}
+                  placeholder="Incharge2 Number"
+                  className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+                //required
+                />
+              </div>
+            </div>
 
             <button
               type="submit"
