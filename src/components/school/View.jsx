8171209d4f0823c759c37caa62aebf38eba4b2a0
Shihab-md <nmd.shihabudeen@gmail.com>
2025-02-24 01:40:47 +0900

    Update View.jsx

--- src/components/school/View.jsx
+++ src/components/school/View.jsx
@@ -36,7 +36,7 @@ const View = () => {
           <h2 className="text-2xl font-bold mb-8 text-center">
             School Details
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
             <div />
             <div>
               <p className="text-lg font-bold text-left">Code:</p>
@@ -44,6 +44,7 @@ const View = () => {
             <div>
               <p className="font-medium text-left">{school.code}</p>
             </div>
+            <div />
             <div>
               <div className="flex space-x-3 mb-5">
                 <p className="text-lg font-bold">Code:</p>
