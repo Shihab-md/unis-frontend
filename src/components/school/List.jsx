0943f4941ae1a53d5b5530541b30555041b7e001
Shihab-md <nmd.shihabudeen@gmail.com>
2025-02-24 03:20:57 +0900

    cnh

--- src/components/school/List.jsx
+++ src/components/school/List.jsx
@@ -5,62 +5,67 @@ import DataTable from 'react-data-table-component'
 import axios from 'axios'
 
 const List = () => {
-    const [schools, setSchools] = useState([])
-    const [schLoading, setSchLoading] = useState(false)
-    const [filteredSchool, setFilteredSchools] = useState(null)
+  const [schools, setSchools] = useState([])
+  const [schLoading, setSchLoading] = useState(false)
+  const [filteredSchool, setFilteredSchools] = useState(null)
 
-    useEffect(() => {
-        const fetchSchools = async () => {
-            setSchLoading(true)
-          try {
-            const responnse = await axios.get(
-              "https://unis-server.vercel.app/api/school",
-              {
-                headers: {
-                  Authorization: `Bearer ${localStorage.getItem("token")}`,
-                },
-              }
-            );
-            if (responnse.data.success) {
-              let sno = 1;
-              const data = await responnse.data.schools.map((sch) => ({
-                _id: sch._id,
-                sno: sno++,
-                code: sch.code,
-                name: sch.name,
-                address: sch.address,
-                action: (<SchoolButtons Id={sch._id} />),
-              }));
-              setSchools(data);
-              setFilteredSchools(data)
-            }
-          } catch (error) {
-            console.log(error.message)
-            if(error.response && !error.response.data.success) {
-              alert(error.response.data.error)
+  useEffect(() => {
+
+    const onSchoolDelete = () => {
+      fetchSchools()
+    }
+
+    const fetchSchools = async () => {
+      setSchLoading(true)
+      try {
+        const responnse = await axios.get(
+          "https://unis-server.vercel.app/api/school",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
           }
-          } finally {
-            setSchLoading(false)
-          }
-        };
-    
-        fetchSchools();
-      }, []);
-
-      const handleFilter = (e) => {
-        const records = schools.filter((sch) => (
-          sch.name.toLowerCase().includes(e.target.value.toLowerCase())
-        ))
-        setFilteredSchools(records)
+        );
+        if (responnse.data.success) {
+          let sno = 1;
+          const data = await responnse.data.schools.map((sch) => ({
+            _id: sch._id,
+            sno: sno++,
+            code: sch.code,
+            name: sch.name,
+            address: sch.address,
+            action: (<SchoolButtons Id={sch._id} onSchoolDelete={onSchoolDelete} />),
+          }));
+          setSchools(data);
+          setFilteredSchools(data)
+        }
+      } catch (error) {
+        console.log(error.message)
+        if (error.response && !error.response.data.success) {
+          alert(error.response.data.error)
+        }
+      } finally {
+        setSchLoading(false)
       }
+    };
 
-      if(!filteredSchool) {
-        return <div>Loading ...</div>
-      }
+    fetchSchools();
+  }, []);
+
+  const handleFilter = (e) => {
+    const records = schools.filter((sch) => (
+      sch.name.toLowerCase().includes(e.target.value.toLowerCase())
+    ))
+    setFilteredSchools(records)
+  }
+
+  if (!filteredSchool) {
+    return <div>Loading ...</div>
+  }
 
   return (
     <div className='p-6'>
-        <div className="text-center">
+      <div className="text-center">
         <h3 className="text-2xl font-bold">Manage School</h3>
       </div>
       <div className="flex justify-between items-center">
@@ -78,7 +83,7 @@ const List = () => {
         </Link>
       </div>
       <div className='mt-6'>
-        <DataTable columns={columns} data={filteredSchool} pagination/>
+        <DataTable columns={columns} data={filteredSchool} pagination />
       </div>
     </div>
   )
--- src/utils/SchoolHelper.jsx
+++ src/utils/SchoolHelper.jsx
@@ -55,8 +55,31 @@ export const getSchools = async (id) => {
   return schools;
 };
 
-export const SchoolButtons = ({ Id }) => {
+export const SchoolButtons = ({ Id, onSchoolDelete }) => {
   const navigate = useNavigate();
+
+  const handleDelete = async (id) => {
+    const confirm = window.confirm("Do you want to delete?");
+    if (confirm) {
+      try {
+        const responnse = await axios.delete(
+          `https://unis-server.vercel.app/api/school/${id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        if (responnse.data.success) {
+          onDepartmentDelete();
+        }
+      } catch (error) {
+        if (error.response && !error.response.data.success) {
+          alert(error.response.data.error);
+        }
+      }
+    }
+  };
 
   return (
     <div className="flex space-x-3">
@@ -71,6 +94,12 @@ export const SchoolButtons = ({ Id }) => {
         onClick={() => navigate(`/admin-dashboard/schools/edit/${Id}`)}
       >
         Edit
+      </button>
+      <button
+        className="px-3 py-1 bg-red-600 text-white"
+        onClick={() => handleDelete(Id)}
+      >
+        Delete
       </button>
     </div>
   );
