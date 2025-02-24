69790dc37bdbcbd5fd95a8673599a7f59f256b85
Shihab-md <nmd.shihabudeen@gmail.com>
2025-02-24 18:43:14 +0900

    Update List.jsx

--- src/components/school/List.jsx
+++ src/components/school/List.jsx
@@ -34,7 +34,7 @@ const List = () => {
             code: sch.code,
             name: sch.name,
             address: sch.address,
-            active: schLoading.active,
+            active: sch.active,
             action: (<SchoolButtons Id={sch._id} onSchoolDelete={onSchoolDelete} />),
           }));
           setSchools(data);
