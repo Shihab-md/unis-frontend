65bd20ce5db148ab4d3b8bba0712c2e6aed561ac
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-01 23:21:20 +0900

    Update Edit.jsx

--- src/components/supervisor/Edit.jsx
+++ src/components/supervisor/Edit.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 //import { fetchDepartments } from "../../utils/SupervisorHelper";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-import { Moment } from 'moment'
+import { Moment } from "moment";
 
 
 const Edit = () => {
@@ -228,7 +228,7 @@ const Edit = () => {
                 <input
                   type="date"
                   name="dob"
-                  value={Moment(new Date(supervisor.dob)).format("yyyy-mm-dd")}
+                  //value={Moment(new Date(supervisor.dob)).format("yyyy-mm-dd")}
                   onChange={handleChange}
                   placeholder="DOB"
                   className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
