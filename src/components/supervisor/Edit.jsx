a8bfa45e3717716eafaaed7d27825e47254852b2
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-01 23:28:54 +0900

    Update Edit.jsx

--- src/components/supervisor/Edit.jsx
+++ src/components/supervisor/Edit.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 //import { fetchDepartments } from "../../utils/SupervisorHelper";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-import { Moment } from "react-moment";
+import moment from 'react-moment';
 
 
 const Edit = () => {
