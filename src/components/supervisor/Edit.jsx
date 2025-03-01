96c36445429e3715fef0c5ad953b282478badf92
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-01 23:15:38 +0900

    Update Edit.jsx

--- src/components/supervisor/Edit.jsx
+++ src/components/supervisor/Edit.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 //import { fetchDepartments } from "../../utils/SupervisorHelper";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-import Moment from 'moment';
+import { Moment } from 'moment'
+
 
 const Edit = () => {
   const [supervisor, setSupervisor] = useState({
