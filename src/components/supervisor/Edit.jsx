9c6de706c20d9528e3ea25985896b5a17d3dd9b5
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-01 20:33:58 +0900

    Update Edit.jsx

--- src/components/supervisor/Edit.jsx
+++ src/components/supervisor/Edit.jsx
@@ -226,7 +226,7 @@ const Edit = () => {
                 <input
                   type="date"
                   name="dob"
-                  value={new Date(supervisor.dob).toString()}
+                  value={new Date(supervisor.dob)}
                   onChange={handleChange}
                   placeholder="DOB"
                   className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
