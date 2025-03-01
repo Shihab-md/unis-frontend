5bd9572ff129fe8d431237b3e230bce743aca069
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-02 05:48:26 +0900

    Update Add.jsx

--- src/components/supervisor/Add.jsx
+++ src/components/supervisor/Add.jsx
@@ -15,7 +15,7 @@ const Add = () => {
 
       const file = files[0];
       const base64 = convertToBase64(file);
-      alert(base64.resolve);
+      alert(base64.Promise.getItem());
       setFormData((prevData) => ({ ...prevData, [name]: base64 }));
 
       console.log(base64)
