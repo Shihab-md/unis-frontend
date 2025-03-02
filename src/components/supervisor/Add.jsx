52e82124eddcf1fc40f42e8defe7500eb4cc72d9
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-02 16:43:49 +0900

    Update Add.jsx

--- src/components/supervisor/Add.jsx
+++ src/components/supervisor/Add.jsx
@@ -12,7 +12,7 @@ const Add = () => {
     const { name, value, files } = e.target;
 
     if (name === "profileImage") {
-      setFormData((prevData) => ({ ...prevData, [name]: files[0] }));
+      //setFormData((prevData) => ({ ...prevData, [name]: files[0] }));
       alert("Hi");
       //console.log(base64)
     } else {
@@ -296,7 +296,7 @@ const Add = () => {
             <label className="block text-sm font-medium text-gray-700">
               Upload Image
             </label>
-            <FileBase64 type="file" className="mt-1 p-2 block w-full border border-gray-300 rounded-md" multiple={true} onDone={({ base64 }) => setFormData({ profileImage: base64 })} onChange={handleChange} />
+            <FileBase64 type="file" className="mt-1 p-2 block w-full border border-gray-300 rounded-md" multiple={false} onDone={({ base64 }) => setFormData({ profileImage: base64 })} onChange={(e) => e.target.files[0]} />
             {/*  <input
               type="file"
               name="profileImage"
