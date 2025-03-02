83f49145b7d69836894da27bb8272d92a06110bf
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-02 16:57:36 +0900

    Update Add.jsx

--- src/components/supervisor/Add.jsx
+++ src/components/supervisor/Add.jsx
@@ -258,7 +258,7 @@ const Add = () => {
             <label className="block text-sm font-medium text-gray-700">
               Upload Image
             </label>
-            <FileBase64 type="file" className="mt-1 p-2 block w-full border border-gray-300 rounded-md" multiple={false} onDone={({ base64 }) => setFormData((prevData) => ({ ...prevData, [profileImage]: base64 }))} onChange={(e) => e.target.files[0]} />
+            <FileBase64 type="file" name="profileImage" className="mt-1 p-2 block w-full border border-gray-300 rounded-md" multiple={false} onDone={({ base64 }) => setFormData((prevData) => ({ ...prevData, [profileImage]: base64 }))} onChange={handleChange} />
             
                      
            {/*  
