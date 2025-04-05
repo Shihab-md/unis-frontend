2173432981e0db655f0a9644d8faf7e44f472124
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-06 04:37:38 +0900

    Update Edit.jsx

--- src/components/school/Edit.jsx
+++ src/components/school/Edit.jsx
@@ -101,8 +101,9 @@ const Edit = () => {
       {school ? (
         <div className="max-w-4xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md">
           <div className="w-full mt-6 bg-teal-700 text-white font-bold py-2 px-4 rounded">
-          <h2 className="text-xl font-bold mb-6">Update School</h2></div>
+          <h2 className="text-xl font-bold text-center">Update School</h2></div>
           <form onSubmit={handleSubmit}>
+          <div className="flex space-x-3 mb-5" />
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               {/* Code */}
               <div>
