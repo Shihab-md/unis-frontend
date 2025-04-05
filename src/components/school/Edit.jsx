1fd410ff6f5ef96f01ea3da5862893bf87dcbc42
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-06 04:35:26 +0900

    ss

--- src/components/school/Edit.jsx
+++ src/components/school/Edit.jsx
@@ -100,7 +100,8 @@ const Edit = () => {
     <>
       {school ? (
         <div className="max-w-4xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md">
-          <h2 className="text-2xl font-bold mb-6">Update School</h2>
+          <div className="w-full mt-6 bg-teal-700 text-white font-bold py-2 px-4 rounded">
+          <h2 className="text-xl font-bold mb-6">Update School</h2></div>
           <form onSubmit={handleSubmit}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               {/* Code */}
@@ -181,7 +182,7 @@ const Edit = () => {
                   name="address"
                   value={school.address}
                   onChange={handleChange}
-                  placeholder="Insert Address"
+                //  placeholder="Insert Address"
                   className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
                   required
                 />
@@ -197,7 +198,7 @@ const Edit = () => {
                   name="district"
                   value={school.district}
                   onChange={handleChange}
-                  placeholder="Insert District / State"
+                //  placeholder="Insert District / State"
                   className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
                   required
                 />
@@ -213,7 +214,7 @@ const Edit = () => {
                   name="contactNumber"
                   value={school.contactNumber}
                   onChange={handleChange}
-                  placeholder="Insert Contact Number"
+                //  placeholder="Insert Contact Number"
                   className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
                 //required
                 />
@@ -229,7 +230,7 @@ const Edit = () => {
                   name="email"
                   value={school.email}
                   onChange={handleChange}
-                  placeholder="Insert Email"
+                //  placeholder="Insert Email"
                   className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
                 //required
                 />
