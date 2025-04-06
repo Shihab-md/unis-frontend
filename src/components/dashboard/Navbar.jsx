7fd1e0799f9b4d2a9ec4c9c386ba58a3cb1d24fb
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-06 21:38:05 +0900

    Update Navbar.jsx

--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -17,8 +17,9 @@ const Navbar = () => {
     <div className='flex items-center text-white justify-between h-12 bg-teal-600 px-5'>
       <Link to="/admin-dashboard" >
         <FaHome />
+      
+      <p className="font-bold">UNIS MS</p>
       </Link>
-      <p className="font-bold px-1">UNIS MS</p>
       <p >     </p>
       <p >Welcome, {user.name}</p>
       <button className='px-4 py-1 bg-teal-700 hover:bg-teal-800' onClick={logout}>Logout</button>
