d000cc1a28b437ef11c28e28f7ad66d2c746cd00
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-06 21:05:34 +0900

    fdsfds

--- src/components/dashboard/AdminSummary.jsx
+++ src/components/dashboard/AdminSummary.jsx
@@ -44,7 +44,7 @@ const AdminSummary = () => {
 
   return (
     <div className="p-6">
-      <h3 className="text-2xl font-bold">Dashboard Overview</h3>
+      <h3 className="text-2xl font-bold">Dashboard</h3>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
         <Link to="/admin-dashboard/supervisors" >
