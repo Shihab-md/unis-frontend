8bd43f86ce1dc23991292efc2f34a9652ed3699d
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-06 21:16:57 +0900

    Update Navbar.jsx

--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -5,7 +5,9 @@ const Navbar = () => {
   const { user, logout } = useAuth()
   return (
     <div className='flex items-center text-white justify-between h-12 bg-teal-600 px-5'>
-      <p className="font-bold">UNIS MS</p>
+      <Link to="/admin-dashboard" >
+        <p className="font-bold">UNIS MS</p>
+      </Link>
       <p >     </p>
       <p >Welcome, {user.name}</p>
       <button className='px-4 py-1 bg-teal-700 hover:bg-teal-800' onClick={logout}>Logout</button>
