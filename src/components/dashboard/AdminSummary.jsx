9d45faafb5b5e0cf3db0abac400c819107bc7456
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-07 21:26:34 +0900

    Update AdminSummary.jsx

--- src/components/dashboard/AdminSummary.jsx
+++ src/components/dashboard/AdminSummary.jsx
@@ -46,7 +46,7 @@ const AdminSummary = () => {
 //p-6
   return (
     <div className="p-5 items-center">
-      <p >Welcome, {user.name}</p>
+      <p classname="text-center">Welcome, {user.name}</p>
       <p ></p>
       <h3 className="text-2xl font-bold">Dashboard</h3>
 
