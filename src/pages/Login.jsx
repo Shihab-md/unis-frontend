2955f0cd1f624f62efe659f5d06ef014d893c238
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-07 22:16:38 +0900

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,8 +41,8 @@ const Login = () => {
       className="flex flex-col items-center h-screen justify-center 
     bg-gradient-to-b from-teal-600 from-50% to-gray-100 to-50% space-y-6"
     >
-      <img width={140} className='rounded-md shadow w-80' src="./Logo - UNIS.PNG" />
-      <h2 className="font-pacific font-bold text-2xl text-pink-700">
+      <img width={140} className='rounded-md shadow p-5 w-34' src="./Logo - UNIS.PNG" />
+      <h2 className="font-pacific font-bold text-3xl text-pink-700">
         UNIS APP
       </h2>
       <div className="border shadow p-6 w-80 bg-white">
