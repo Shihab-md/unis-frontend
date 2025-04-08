2a88b747cbfe5ec71f8e7a2c799641d411f47273
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-08 20:55:37 +0900

    111

--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -10,11 +10,11 @@ const Navbar = () => {
   return (
     <div className='flex items-center text-white justify-between h-12 bg-teal-600 px-5'>
       <Link to="/admin-dashboard" >
-        <FaHome className="text-2xl shadow" />
+        <FaHome className="text-2xl shadow text-blue-700" />
       </Link>
       <p className="text-xl font-bold">UNIS APP</p>
-      <FaPowerOff className="text-2xl shadow" onClick={logout} />
-      <button className='px-4 py-1 bg-teal-700 hover:bg-teal-800' onClick={logout}>Logout</button>
+      <FaPowerOff className="text-2xl shadow text-red-700" onClick={logout} />
+      // <button className='px-4 py-1 bg-teal-700 hover:bg-teal-800' onClick={logout}>Logout</button>
     </div>
   )
 }
