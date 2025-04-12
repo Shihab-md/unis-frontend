07d2e6f739b8e260995b14fdf24a21fa78d5fbfe
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-12 14:50:04 +0900

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -49,7 +49,7 @@ const Login = () => {
         UNIS ACADEMY
       </h2>
       <div className="border shadow p-6 w-80 bg-white">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-4">
           <h2 className="text-2xl font-bold mb-4">Login </h2>
           <FaKey />
         </div>
