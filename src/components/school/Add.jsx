64e6ea8687f794df3147f124a6742f674363ef7a
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-12 12:08:24 +0900

    Update Add.jsx

--- src/components/school/Add.jsx
+++ src/components/school/Add.jsx
@@ -51,11 +51,11 @@ const Add = () => {
     <div className="max-w-4xl mx-auto mt-2 bg-white p-5 rounded-md shadow-md">
       <div className="grid items-center justify-end">
         <Link to="/admin-dashboard/schools" >
-          <FaWindowClose className="text-2xl bg-red-700 text-white rounded" />
+          <FaWindowClose className="text-2xl bg-red-700 text-white rounded shadow-md" />
         </Link>
       </div>
       <div className="grid grid-cols-1 mt-3 bg-teal-700 text-white font-bold py-2 px-4 rounded">
-        <div><h2 className="grid text-xl font-bold items-center justify-center">Add Niswan</h2></div>
+        <div><h2 className="grid text-xl font-bold items-center justify-center">Enter Niswan Details</h2></div>
       </div>
 
       <form onSubmit={handleSubmit}>
@@ -428,14 +428,6 @@ const Add = () => {
             //required
             />
           </div>
-
-          <button
-            data-ripple-light="true"
-            className="w-full mt-6 bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => navigate(`/admin-dashboard/schools`)}
-          >
-            Cancel
-          </button>
           <button
             type="submit"
             data-ripple-light="true"
