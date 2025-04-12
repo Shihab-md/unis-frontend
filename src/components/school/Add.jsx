dfafd5578e6660ea70a7a6ebd2c8ec78c85b27e9
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-12 11:33:49 +0900

    Update Add.jsx

--- src/components/school/Add.jsx
+++ src/components/school/Add.jsx
@@ -49,10 +49,12 @@ const Add = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto mt-10 bg-white p-5 rounded-md shadow-md">
-      <Link to="/admin-dashboard/schools" >
+    <div className="max-w-4xl mx-auto mt-2 bg-white p-5 rounded-md shadow-md">
+      <div className="grid items-center justify-end">
+        <Link to="/admin-dashboard/schools" >
           <FaArrowAltCircleLeft className="text-2xl bg-blue-700 text-white rounded" />
         </Link>
+      </div>
       <div className="grid grid-cols-3 mt-6 bg-teal-700 text-white font-bold py-2 px-4 rounded">
         <div><h2 className="grid text-xl font-bold items-center justify-end">Add</h2></div>
         <div><h2 className="grid text-xl font-bold items-center justify-center">Niswan</h2></div>
