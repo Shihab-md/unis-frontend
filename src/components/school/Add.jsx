91892824a2d391dc0a11518c1d653b33e97cd3c6
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-12 12:36:01 +0900

    qre

--- src/components/school/Add.jsx
+++ src/components/school/Add.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom'
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import {
   FaWindowClose
 } from "react-icons/fa";
