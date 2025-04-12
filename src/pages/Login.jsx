bcb376a50873b779bb288b085eeffcad378eef5d
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-12 19:43:33 +0900

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -43,62 +43,62 @@ const Login = () => {
     <div>
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="mobile-web-app-capable" content="yes" />
-    <div
-      className="flex flex-col items-center h-screen justify-center 
+      <div
+        className="flex flex-col items-center h-screen justify-center 
       bg-gradient-to-b from-teal-600 from-50% to-gray-100 to-50% space-y-6 h-75"
-    >
-      <img width={140} className='rounded-md shadow w-34' src="./Logo - UNIS.PNG" />
-      <h2 className="p-5 font-bold text-4xl">
-        UNIS ACADEMY
-      </h2>
-      <div className="border shadow p-6 w-80 bg-white">
-        <h2 className="text-2xl font-bold mb-4">Login</h2>
-        {error && <p className="text-red-500">{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700">
-              Email
-            </label>
-            <input
-              type="email"
-              className="w-full px-3 py-2 border"
-              placeholder="Enter Email"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-700">
-              Password
-            </label>
-            <input
-              type="password"
-              className="w-full px-3 py-2 border"
-              placeholder="*****"
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4 flex items-center justify-between">
-            <label className="inline-flex items-center">
-              <input type="checkbox" className="form-checkbox" />
-              <span className="ml-2 text-gray-700">Remember me</span>
-            </label>
-            <a href="#" className="text-teal-600">
-              Forgot password?
-            </a>
-          </div>
-          <div className="mb-4">
-            <button
-              type="submit"
-              className="w-full bg-teal-600 text-white py-2 "
-            >
-              Login (بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ) <FaArrowAltCircleRight/>
-            </button>
-          </div>
-        </form>
+      >
+        <img width={140} className='rounded-md shadow w-34' src="./Logo - UNIS.PNG" />
+        <h2 className="p-5 font-bold text-4xl">
+          UNIS ACADEMY
+        </h2>
+        <div className="border shadow p-6 w-80 bg-white">
+          <h2 className="text-2xl font-bold mb-4">Login</h2>
+          {error && <p className="text-red-500">{error}</p>}
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">
+                Email
+              </label>
+              <input
+                type="email"
+                className="w-full px-3 py-2 border"
+                placeholder="Enter Email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-gray-700">
+                Password
+              </label>
+              <input
+                type="password"
+                className="w-full px-3 py-2 border"
+                placeholder="*****"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-4 flex items-center justify-between">
+              <label className="inline-flex items-center">
+                <input type="checkbox" className="form-checkbox" />
+                <span className="ml-2 text-gray-700">Remember me</span>
+              </label>
+              <a href="#" className="text-teal-600">
+                Forgot password?
+              </a>
+            </div>
+            <div className="mb-4">
+              <button
+                type="submit"
+                className="w-full bg-teal-600 text-white py-2 "
+              >
+                بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْم <FaArrowAltCircleRight />
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
