37c3bc3126e286841c70f2791ccd656e1edd817b
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-12 13:39:52 +0900

    Update AdminSummary.jsx

--- src/components/dashboard/AdminSummary.jsx
+++ src/components/dashboard/AdminSummary.jsx
@@ -46,7 +46,7 @@ const AdminSummary = () => {
       <h5 className="p-3 text-center">Welcome, {user.name}</h5>
       <h3 className="text-2xl font-bold text-center">Dashboard</h3>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5 rounded-md shadow-md">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5 rounded">
         <Link to="/admin-dashboard/supervisors" >
           <SummaryCard
             icon={<FaUserFriends />}
