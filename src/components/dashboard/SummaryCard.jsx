eb052082499fa6c6513ea0ec034355000366e779
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-13 16:56:52 +0900

    iugiu

--- src/components/dashboard/SummaryCard.jsx
+++ src/components/dashboard/SummaryCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SummaryCard = ({icon, text, number, color}) => {
   return (
     <div className="rounded flex bg-white border">
-        <div className={`text-3xl flex justify-center items-center ${color} text-white px-3`}>
+        <div className={`text-3xl flex justify-center items-center ${color} text-white px-2`}>
             {icon}
         </div>
         <div className="pl-3 py-1">
