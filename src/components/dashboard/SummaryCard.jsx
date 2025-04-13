94c43e5a1bb85001d5c5b890394015d9b4fd7649
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-13 17:44:26 +0900

    sdfsf

--- src/components/dashboard/SummaryCard.jsx
+++ src/components/dashboard/SummaryCard.jsx
@@ -6,7 +6,7 @@ const SummaryCard = ({icon, text, number, color}) => {
         <div className={`text-3xl flex justify-center items-center ${color} text-white px-2`}>
             {icon}
         </div>
-        <div className="pl-3 py-1">
+        <div className="pl-2 py-1">
             <p className="font-semibold">{text}</p>
             <p className="font-bold text-gray-500">{number}</p>
         </div>
