cb6f5f4f7214af6de2492e1c939d4579837b6288
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-15 13:43:46 +0900

    saa

--- src/components/dashboard/SummaryCard.jsx
+++ src/components/dashboard/SummaryCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SummaryCard = ({icon, text, number, color}) => {
   return (
     <div className="rounded flex bg-white border shadow-lg">
-        <div className={`text-3xl flex justify-center items-center ${color} text-gray-100 px-2 rounded-l-lg`}>
+        <div className={`text-2xl flex justify-center items-center ${color} text-gray-100 px-2 rounded-l-lg`}>
             {icon}
         </div>
         <div className="pl-2 py-1 rounded-r-lg">
