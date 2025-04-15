458541490c8019121245c58db3d45e2c91c9c072
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-15 15:52:49 +0900

    asdsadas

--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
   return (
     <div className='flex items-center text-white justify-between h-12 bg-teal-600 px-5 text-shadow-lg'>
       <Link to="/admin-dashboard" >
-        <FaHome className="text-2xl text-white text-shadow-lg" />
+        <FaHome className="text-2xl text-yellow-200 text-shadow-lg" />
       </Link>
       <p className="text-xl font-bold text-shadow-lg">UNIS ACADEMY</p>
-      <FaSignOutAlt className="text-2xl text-red-700 text-shadow-lg" onClick={logout} />
+      <FaSignOutAlt className="text-2xl text-red-500 text-shadow-lg" onClick={logout} />
       {/*<button className='px-4 py-1 bg-teal-700 hover:bg-teal-800' onClick={logout}>Logout</button> */}
     </div>
   )
--- src/components/institute/View.jsx
+++ src/components/institute/View.jsx
@@ -39,7 +39,7 @@ const View = () => {
         <div className="max-w-3xl mx-auto mt-2 bg-white p-8 rounded-md shadow-md">
           <div className="flex py-2 px-4 items-center justify-center bg-teal-700 text-white rounded-lg shadow-lg">
             <h2 className="text-xl font-semibold items-center justify-center">Institute Details</h2>
-            <Link to="/admin-dashboard/supervisors" >
+            <Link to="/admin-dashboard/institutes" >
               <FaRegTimesCircle className="text-2xl ml-7 text-red-700 bg-gray-200 rounded-xl shadow-md items-center justify-end" />
             </Link>
           </div>
