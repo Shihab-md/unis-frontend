0188afd83f15257793c8ab46c66ea13567c28c49
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-15 15:25:48 +0900

    Update Add.jsx

--- src/components/supervisor/Add.jsx
+++ src/components/supervisor/Add.jsx
@@ -4,8 +4,6 @@ import { useNavigate, Link } from "react-router-dom";
 import {
   FaRegTimesCircle
 } from "react-icons/fa";
-///import multer from "multer";
-//import { FileBase64 } from "react-file-base64";
 
 const Add = () => {
 
@@ -65,17 +63,12 @@ const Add = () => {
 
   return (
     <div className="max-w-4xl mx-auto mt-2 bg-white p-5 rounded-md shadow-md">
-      <div className="grid items-center justify-end px-1 py-1">
-        <Link to="/admin-dashboard/supervisors" >
-          <FaRegTimesCircle className="text-xl bg-red-700 text-white rounded shadow-md" />
-        </Link>
-      </div>
       {/*<div className="grid grid-cols-1 mt-2 bg-teal-700 text-white font-bold py-2 px-4 rounded"> */}
-        <div className="flex py-2 px-4 items-center justify-center bg-teal-700 text-white rounded-lg">
-          <h2 className="text-xl font-semibold items-center justify-center">Enter Supervisor Details</h2>
-          <Link to="/admin-dashboard/supervisors" >
-            <FaRegTimesCircle className="text-2xl ml-7 mt-1 text-red-700 bg-gray-200 rounded-xl shadow-md items-center justify-end" />
-          </Link></div>
+      <div className="flex py-2 px-4 items-center justify-center bg-teal-700 text-white rounded-lg shadow-lg">
+        <h2 className="text-xl font-semibold items-center justify-center">Enter Supervisor Details</h2>
+        <Link to="/admin-dashboard/supervisors" >
+          <FaRegTimesCircle className="text-2xl ml-7 text-red-700 bg-gray-200 rounded-xl shadow-md items-center justify-end" />
+        </Link></div>
 
       <form onSubmit={handleSubmit}>
         <div className="flex space-x-3 mb-5" />
