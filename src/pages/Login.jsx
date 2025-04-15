aaf70f3eaaf8587e2e5092ca973b703fed34f87c
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-15 16:29:38 +0900

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -108,7 +108,7 @@ const Login = () => {
             <div className="mb-3">
               <button
                 type="submit"
-                className="flex w-full bg-teal-600 text-white py-2 items-center justify-center shadow-lg"
+                className="flex w-full bg-teal-600 text-white py-2 items-center justify-center rounded-lg shadow-xl"
               >
                 بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْم
               </button>
