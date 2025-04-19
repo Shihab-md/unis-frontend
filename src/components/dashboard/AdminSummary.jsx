9d1aa07bc0cfe62b618fdaf20278324982ec4eb5
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-19 21:10:36 +0900

    Update AdminSummary.jsx

--- src/components/dashboard/AdminSummary.jsx
+++ src/components/dashboard/AdminSummary.jsx
@@ -45,10 +45,10 @@ const AdminSummary = () => {
   }
   //p-6
   return (
-    <div className="p-7 pl-3 items-center rounded-lg">
+    <div className="p-7 pt-3 items-center rounded-lg">
       <h5 className="p-1 text-center font-semibold text-gray-700">ربِّ زِدْنِي عِلْماً</h5>
       <h5 className="p-1 text-center">Welcome, {user.name}</h5>
-      <h5 className="text-xl mt-3 mb-7 font-bold text-center text-gray-500 text-shadow-lg">Super Admin Dashboard</h5>
+      <h5 className="text-xl mt-2 mb-5 font-bold text-center text-gray-500 text-shadow-lg">Super Admin Dashboard</h5>
 
       <div className="rounded-lg grid grid-cols-2 md:grid-cols-4 gap-7 mt-10 flex rounded-lg">
         <Link to="/admin-dashboard/supervisors" >
