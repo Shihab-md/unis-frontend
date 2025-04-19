785c7ab8dbd5ee040e14eaa673f73eb918e344f0
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-19 22:11:40 +0900

    Update List.jsx

--- src/components/student/List.jsx
+++ src/components/student/List.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { columns, SudentButtons } from '../../utils/SudentHelper'
+import { columns, StudentButtons } from '../../utils/StudentHelper'
 import DataTable from 'react-data-table-component'
 import axios from 'axios'
 import {
