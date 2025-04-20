c75021a84624b1cc7f0c5b22d9201a3574ad03f4
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-20 20:56:27 +0900

    Update Add.jsx

--- src/components/student/Add.jsx
+++ src/components/student/Add.jsx
@@ -546,7 +546,9 @@ const Add = () => {
                   <input
                     type="number"
                     name="fees1"
-                    value={courses.map().find(courseId1Val).fees}
+                    value={courses
+                      .filter(course => course._id === courseId1Val)
+                      .map(course => course.fees)}
                     disabled={true}
                     onChange={handleChange}
                     className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
