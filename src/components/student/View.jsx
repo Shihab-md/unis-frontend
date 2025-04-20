9d601d54c00b99eb570e16fdc98db2a4f9556986
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-20 15:15:28 +0900

    Update View.jsx

--- src/components/student/View.jsx
+++ src/components/student/View.jsx
@@ -7,11 +7,11 @@ import {
 
 const View = () => {
   const { id } = useParams();
-  const [student, setSudent] = useState(null);
+  const [student, setStudent] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchSudent = async () => {
+    const fetchStudent = async () => {
       try {
         const responnse = await axios.get(
           `https://unis-server.vercel.app/api/student/${id}`,
@@ -22,7 +22,7 @@ const View = () => {
           }
         );
         if (responnse.data.success) {
-          setSudent(responnse.data.student);
+          setStudent(responnse.data.student);
         }
       } catch (error) {
         if (error.response && !error.response.data.success) {
@@ -31,14 +31,14 @@ const View = () => {
       }
     };
 
-    fetchSudent();
+    fetchStudent();
   }, []);
   return (
     <>
       {student ? (
         <div className="max-w-3xl mx-auto mt-2 bg-white p-8 rounded-md shadow-md">
           <div className="flex py-2 px-4 items-center justify-center bg-teal-700 text-white rounded-lg shadow-lg">
-            <h2 className="text-xl font-semibold items-center justify-center">Staff Details</h2>
+            <h2 className="text-xl font-semibold items-center justify-center">Student Details</h2>
             <Link to="/admin-dashboard/students" >
               <FaRegTimesCircle className="text-2xl ml-7 text-red-700 bg-gray-200 rounded-xl shadow-md items-center justify-end" />
             </Link>
@@ -55,43 +55,27 @@ const View = () => {
               </div>
               <div>
                 <div className="flex space-x-3 mb-5" />
-                <div className="flex space-x-3 mb-5">
-                  <p className="font-medium">Name:</p>
-                  <p className="font-normal">{student.userId.name}</p>
-                </div>
-                <div className="flex space-x-3 mb-5">
-                  <p className="font-medium">Email:</p>
-                  <p className="font-normal">{student.userId.email}</p>
-                </div>
-                <div className="flex space-x-3 mb-5">
-                  <p className="font-medium">Sudent ID:</p>
-                  <p className="font-normal">{student.studentId}</p>
-                </div>
+
                 <div className="flex space-x-3 mb-5">
                   <p className="font-medium">Niswan Name:</p>
                   <p className="font-normal">{student.schoolId.nameEnglish}</p>
                 </div>
                 <div className="flex space-x-3 mb-5">
-                  <p className="font-medium">Contact Number:</p>
-                  <p className="font-normal">{student.contactNumber}</p>
+                  <p className="font-medium">Roll Number:</p>
+                  <p className="font-normal">{student.rollNumber}</p>
                 </div>
                 <div className="flex space-x-3 mb-5">
-                  <p className="font-medium">Address:</p>
-                  <p className="font-normal">{student.address}</p>
+                  <p className="font-medium">Name:</p>
+                  <p className="font-normal">{student.userId.name}</p>
                 </div>
+                
                 <div className="flex space-x-3 mb-5">
-                  <p className="font-medium">Designation:</p>
-                  <p className="font-normal">{student.designation}</p>
-                </div>
-                <div className="flex space-x-3 mb-5">
-                  <p className="font-medium">Qualification:</p>
-                  <p className="font-normal">{student.qualification}</p>
+                  <p className="font-medium">Date of Addmission:</p>
+                  <p className="font-normal">{new Date(student.doa).toLocaleDateString()}</p>
                 </div>
                 <div className="flex space-x-3 mb-5">
                   <p className="font-medium">Date of Birth:</p>
-                  <p className="font-normal">
-                    {new Date(student.dob).toLocaleDateString()}
-                  </p>
+                  <p className="font-normal">{new Date(student.dob).toLocaleDateString()}</p>
                 </div>
                 <div className="flex space-x-3 mb-5">
                   <p className="font-medium">Gender:</p>
@@ -102,15 +86,75 @@ const View = () => {
                   <p className="font-normal">{student.maritalStatus}</p>
                 </div>
                 <div className="flex space-x-3 mb-5">
-                  <p className="font-medium">Date of Joining:</p>
-                  <p className="font-normal">
-                    {new Date(student.doj).toLocaleDateString()}
-                  </p>
+                  <p className="font-medium">Blood Group:</p>
+                  <p className="font-normal">{student.bloodGroup}</p>
                 </div>
                 <div className="flex space-x-3 mb-5">
-                  <p className="font-medium">Salary:</p>
-                  <p className="font-normal">{student.salary}</p>
+                  <p className="font-medium">Identification Mark 1:</p>
+                  <p className="font-normal">{student.idMark1}</p>
                 </div>
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Identification Mark 2:</p>
+                  <p className="font-normal">{student.idMark2}</p>
+                </div>
+
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Father's Name:</p>
+                  <p className="font-normal">{student.fatherName}</p>
+                </div>
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Father's Number:</p>
+                  <p className="font-normal">{student.fatherNumber}</p>
+                </div>
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Father's Occupation:</p>
+                  <p className="font-normal">{student.fatherOccupation}</p>
+                </div>
+
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Mother's Name:</p>
+                  <p className="font-normal">{student.motherName}</p>
+                </div>
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Mother's Number:</p>
+                  <p className="font-normal">{student.motherNumber}</p>
+                </div>
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Mother's Occupation:</p>
+                  <p className="font-normal">{student.motherOccupation}</p>
+                </div>
+
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Guardian's Name:</p>
+                  <p className="font-normal">{student.guardianName}</p>
+                </div>
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Guardian's Number:</p>
+                  <p className="font-normal">{student.guardianNumber}</p>
+                </div>
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Guardian's Occupation:</p>
+                  <p className="font-normal">{student.guardianOccupation}</p>
+                </div>
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Guardian's Relationship:</p>
+                  <p className="font-normal">{student.guardianRelation}</p>
+                </div>
+
+
+
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">Address:</p>
+                  <p className="font-normal">{student.address}</p>
+                </div>
+                <div className="flex space-x-3 mb-5">
+                  <p className="font-medium">State / District:</p>
+                  <p className="font-normal">{student.district}</p>
+                </div>
+                
+                
+                
+                
               </div>
             </div>
           </div>
