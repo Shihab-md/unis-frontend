a3b2dfaa7042983e5faa54dd95f53c600df1085d
Shihab-md <nmd.shihabudeen@gmail.com>
2025-05-04 22:03:02 +0900

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import { getBaseUrl } from '../../utils/CommonHelper'
 
 const userContext = createContext();
 
@@ -13,7 +14,7 @@ const AuthContext = ({ children }) => {
         const token = localStorage.getItem("token");
         if (token) {
           const response = await axios.get(
-            "https://unis-server.vercel.app/api/auth/verify",
+            (await getBaseUrl()).toString() + "auth/verify",
             {
               headers: {
                 Authorization: `Bearer ${token}`,
