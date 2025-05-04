ff84aca1d1f2e89a3fcb89f14a096910d767cb59
Shihab-md <nmd.shihabudeen@gmail.com>
2025-05-04 21:47:16 +0900

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { getBaseUrl } from '../../utils/CommonHelper'
+import { getBaseUrl } from '../utils/CommonHelper'
 
 const userContext = createContext();
 
