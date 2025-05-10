06b043663711ebddc5498ae2a96835869c8b6078
Shihab-md <nmd.shihabudeen@gmail.com>
2025-05-10 23:40:04 +0900

    Update Create.jsx

--- src/components/certificate/Create.jsx
+++ src/components/certificate/Create.jsx
@@ -181,8 +181,8 @@ const Create = () => {
                   Select Students <span className="text-red-700">*</span>
                 </label>
               </div>
-              <div className="flex space-x-3 mb-5" />
-              <div className='mt-2 mb-5 gap-2 border rounded-md shadow-lg'>
+              <div className="flex space-x-3" />
+              <div className='mb-5 gap-2 border rounded-md shadow-lg'>
                 <DataTable columns={columnsSelect} data={students} reloadData={handleReload} selectableRows onSelectedRowsChange={handleRowChange} clearSelectedRows={toggledClearRows} />
               </div>
 
