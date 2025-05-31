600bb4a45c8597a5e7e881621db514a9b53afdc8
Shihab-md <nmd.shihabudeen@gmail.com>
2025-05-31 19:37:11 +0900

    fsdf

--- src/components/institute/Add.jsx
+++ src/components/institute/Add.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 import { getBaseUrl, handleRightClick, checkAuth } from '../../utils/CommonHelper';;
