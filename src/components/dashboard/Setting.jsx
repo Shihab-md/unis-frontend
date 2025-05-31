ddc21dbb0ead325eb120d0a0b3347de981bd09a3
Shihab-md <nmd.shihabudeen@gmail.com>
2025-05-31 23:11:31 +0900

    Update Setting.jsx

--- src/components/dashboard/Setting.jsx
+++ src/components/dashboard/Setting.jsx
@@ -60,7 +60,7 @@ const Setting = () => {
   };
 
   return (
-    <div className="max-w-3xl mx-auto mt-10 bg-white p-7 rounded-md shadow-md">
+    <div className="max-w-3xl mx-auto mt-10 bg-white p-7 rounded-md shadow-md bg-blue-50 ">
       <div className="flex py-2 px-4 items-center justify-center bg-teal-700 text-white rounded-lg shadow-lg">
         <h2 className="text-xl font-semibold items-center justify-center">Change Password</h2>
         <Link to="/dashboard" >
