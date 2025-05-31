567321988898357b2f020369320ac61d71b5dcdd
Shihab-md <nmd.shihabudeen@gmail.com>
2025-05-31 23:52:12 +0900

    sad

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -7,7 +7,7 @@ const AdminDashboard = () => {
   const {user} = useAuth()
  
   return (
-      <div className='flex-1 bg-gray-200 min-h-screen bg-[url(../bg-img.jpg)] bg-fixed bg-cover bg-center bg-repeat bg-teal-50'>
+      <div className='flex-1 bg-gray-200 min-h-screen h-9/10 bg-[url(../bg-img.jpg)] bg-fixed bg-cover bg-center bg-repeat bg-teal-50'>
         <Navbar />
         <Outlet />
       </div>
