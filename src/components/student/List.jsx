1e960589b53e900cd14b78af0a97408d295b7d1e
Shihab-md <nmd.shihabudeen@gmail.com>
2026-01-29 23:40:45 +0900

    Update List.jsx

--- src/components/student/List.jsx
+++ src/components/student/List.jsx
@@ -87,7 +87,7 @@ const List = () => {
 
   function NiswanSelect({ options, onChange, selectedValues }) {
     return <Select options={
-      options.map((option) => ({
+      options.filter(option => option.active === 'Active').map((option) => ({
         value: option._id, label: option.code + " : " + option.nameEnglish
       }
       ))
@@ -701,7 +701,7 @@ const List = () => {
             <div className="mb-2 h-80 w-full">
               <div className='text-2xl lg:text-3xl mb-3 text-blue-600'>Select the Niswan</div>
               <Select className='text-sm text-start'
-                options={schools.filter(school => school.code !== 'UN-00-00001').map(option => ({
+                options={schools.filter((school) => school.code !== 'UN-00-00001' && school.active === 'Active').map(option => ({
                   value: option._id, label: option.code + " : " + option.nameEnglish
                 }))}
                 onChange={(selectedOption) => {
