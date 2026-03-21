9b95de12b3dfa60bb42e8bc7fbdf04a0351d4178
Shihab-md <nmd.shihabudeen@gmail.com>
2026-03-21 21:26:24 +0900

    Update List.jsx

--- src/components/student/List.jsx
+++ src/components/student/List.jsx
@@ -168,7 +168,7 @@ const List = () => {
         <div className="mb-2 h-80 w-full">
           <div className='text-xl font-bold md:mb-1 text-green-600 text-center'>Filter</div>
 
-          <div className='grid grid-cols-4 md:grid-cols-6 gap-x-3 lg:gap-x-5'>
+          <div className='grid grid-cols-4 md:grid-cols-6 gap-x-3 lg:gap-x-3'>
             <span className='col-span-2 md:col-span-3 text-sm mb-1 text-start text-blue-500'>Course</span>
             <span className='text-sm mb-1 text-start text-blue-500'>Year</span>
             <span className='md:col-span-2 text-sm mb-1 text-start text-blue-500'>Course Status</span>
@@ -190,7 +190,13 @@ const List = () => {
               options={[
                 { value: '1', label: '1' },
                 { value: '2', label: '2' },
-                { value: '3', label: '3' }
+                { value: '3', label: '3' },
+                { value: '4', label: '4' },
+                { value: '5', label: '5' },
+                { value: '6', label: '6' },
+                { value: '7', label: '7' },
+                { value: '8', label: '8' },
+                { value: '9', label: '9' }
               ]}
               onChange={(selectedOption) => {
                 selectedYear = selectedOption.value;
